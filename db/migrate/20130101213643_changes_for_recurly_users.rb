b5aa4e3b6630e8758d4e7fcb7f8c173fb035c8ad
Andrew Davis <andrew@andrewrdavis.net>
2013-01-01 16:42:30 -0500

    Update User model

---
+++ db/migrate/20130101213643_changes_for_recurly_users.rb
@@ -0,0 +1,9 @@
+class ChangesForRecurlyUsers < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.rename :name, :first_name
+      t.string :last_name
+      t.string :customer_id
+    end
+  end
+end
