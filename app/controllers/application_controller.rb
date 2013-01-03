85c10777c17b227566ee8be202004aa8f813cb67
Andrew Davis <andrew@andrewrdavis.net>
2013-01-02 22:10:19 -0500

    add Recurly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper :all
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  before_create :check_recurly
+
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -7,10 +10,21 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :customer_id
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :customer_id, :card_token
+  attr_accessor :card_token
 
   def name
     "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
+  private
+
+  def check_recurly
+    Recurly::Account.find(customer_id) unless customer_id.nil?
+  rescue Recurly::Resource::NotFound => e
+    logger.error e.message
+    errors.add :base, "Unable to create your subscription. #{e.message}"
+    false
   end
 
 end
